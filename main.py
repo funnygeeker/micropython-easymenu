12bb621b92afc1c64a92c37620190b6af0e411aa
funnygeeker <funnygeeker@outlook.com>
2023-12-01 00:09:27 +0800

    更新使用示例

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+import time
+import framebuf
+from machine import SPI, Pin
+from drivers import st7789_spi
+from libs.easydisplay import EasyDisplay
+from libs.easymenu import EasyMenu
+
+# ESP32C3 & ST7789
+spi = SPI(1, baudrate=20000000, polarity=0, phase=0, sck=Pin(19), mosi=Pin(18))
+dp = st7789_spi.ST7789(width=240, height=240, spi=spi, cs=0, dc=1, rst=11, rotation=1)
+ed = EasyDisplay(display=dp, font="/font/text_lite_16px_2311.v3.bmf", show=True, color=0xFFFF, clear=True,
+                 color_type=framebuf.RGB565)
 menu = {
-     'img': ('/ing/text.dat', 0, 0),
-     'func': {'show': None, 'back': None, 'move': None, 'up': None, 'down': None, 'left': None, 'right': None, 'enter': None},
-     'text': ('TEXT', 'center', 0),  # 'top', 'bottom'
-     'border': {'reversion': 1, 'func': func},  # func(x0, y0, x1, y1) # 外边框起始和结束点
-     'select': True,
-     'layout': (4, 1),
-     'spacing': (100, 20),
-    0: {'text': ('TEXT', 0, 0),  # align:left or right, only text support
-        'img': ('/ing/text.dat', 0, 0),  # or str
-        'select': True}
-     }
+    'offset': (16, 16),
+    'title': ('Test Menu', 'center', 0),
+    'start': (0, 22),
+    'layout': (2, 2),
+    'spacing': (60, 20),
+    0: {'text': 'TEXT0'},
+    1: {'text': 'TEXT1',
+        0: {'text': ('TEXT1-0', 0, 0)},
+        1: {'text': ('TEXT1-1', 0, 0)}
+        },
+    2: {'text': ('TEXT2', 0, 0)},
+    3: {'text': ('TEXT3', 0, 0)},
+    4: {'text': ('TEXT4', 0, 0)},
+    5: {'text': ('TEXT5', 0, 0)}
+}
+em = EasyMenu(ed, menu)
+time.sleep(3)
+em.prev()  # Previous option
+time.sleep(3)
+em.next()  # Next option
+print(em.select())  # Outputs the content of the current option
